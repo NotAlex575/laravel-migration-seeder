af0dfbeb70dc30748e5fb27bb00d5ae521500311
NotAlex575 <dapontealessandro89gmail.com>
2025-11-21 15:44:25 +0100

    setup componenti view

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts/master")
+
+@section("title")
+    Homepage
+@endsection
+
+@section("main")
+    <h1>SEI NELLA HOMEPAGE!</h1>
+@endsection
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4">
+                <h5>La tua azienda</h5>
+                <p class="small">
+                    Descrizione breve dell’azienda o del sito.  
+                    Puoi inserire anche indirizzo o contatti.
+                </p>
+            </div>
+
+            <div class="col-md-4">
+                <h5>Link utili</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Home</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Chi siamo</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Servizi</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Contatti</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4">
+                <h5>Seguici</h5>
+                <div class="d-flex gap-3">
+                    <a href="#" class="text-white fs-5"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="text-white fs-5"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="text-white fs-5"><i class="bi bi-github"></i></a>
+                </div>
+            </div>
+
+        </div>
+
+        <hr class="border-light">
+
+        <div class="text-center small">
+            © 2025 - Tutti i diritti riservati
+        </div>
+    </div>
+</footer>
