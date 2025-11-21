5aacf6def97bb50845fcd20c9320302b23644353
NotAlex575 <dapontealessandro89gmail.com>
2025-11-21 16:47:44 +0100

    creata nuova migrazione che inserisce alla table totale_posti

---
+++ database/migrations/2025_11_21_154351_add_totale_posti_to_classes_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('classes', function (Blueprint $table) {
+
+            //aggiungiamo una colonna totale_posti che sarà in integer
+            $table->integer("totale_posti");
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('classes', function (Blueprint $table) {
+            $table->dropColumn("totale_posti");
+        });
+    }
+};
